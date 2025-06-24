4f7c5da82498c8a3b1c2c68dde70570c51bc8238
Matei-Gatin <thedackchair@gmail.com>
2025-06-24 13:55:05 +0300

    Production database change

--- TodoApp/database.py
+++ TodoApp/database.py
@@ -2,10 +2,16 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-SQLALCHEMY_DATABASE_URL = \
-    'postgresql://postgres:admin123@localhost/todosapp'
+SQLALCHEMY_DATABASE_URL = "postgresql://todoapp_user:X67VRD9VeNixsc2uuNEZNLoprNkb6gao@dpg-d1d83kndiees73ckipg0-a/todoapp_zluj"
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL
+)
+
+# SQLALCHEMY_DATABASE_URL = \
+#     'postgresql://postgres:admin123@localhost/todosapp'
+#
+# engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False,
                            bind=engine)
